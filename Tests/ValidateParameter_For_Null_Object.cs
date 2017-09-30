785678312edc3b18b1d912ae6c0389f433146552
Brad <brad.whitecross>
2017-09-30 10:09:09 +1000

    Renamed the directory for tests.

--- Tests/ValidateParameter_For_Null_Object.cs
+++ Tests/ValidateParameter_For_Null_Object.cs
@@ -8,6 +8,15 @@ namespace Yelware.Utilities.FluentValidationTests
     public class ValidateParameter_For_Null_Object
     {
         [Fact]
+        public void Validate_Parameter_Throws_ArgumentNullException_For_Null_Name()
+        {
+            Object obj = null;
+            Assert.Throws<ArgumentNullException>(
+                () => Validate.Parameter(obj, null)
+            );
+        }
+
+        [Fact]
         public void Is_With_Not_Null_Test_Throws_ArgumentException()
         {
             Object obj = null;
